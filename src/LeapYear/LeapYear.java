9d848927d6dbe7abf2bf699df8dd61fdee03c911
KurtXanderCabural <Kurtxander.cabural@cit.edu>
2022-12-17 12:08:00 +0800

    progress LeapYear

--- src/LeapYear/LeapYear.java
+++ src/LeapYear/LeapYear.java
@@ -1,12 +1,36 @@
 package LeapYear;
 
 import javax.swing.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class LeapYear extends JFrame {
     private JPanel panel1;
-    private JTextField textField1;
-    private JButton checkButton;
+    private JTextField tfYear;
+    private JButton btnCheckYear;
 
+
+    public LeapYear(){
+        btnCheckYear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int year = Integer.parseInt(tfYear.getText());
+
+                if(isLeap(year)){
+                    JOptionPane.showMessageDialog(null, "Leap Year");
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Not a Leap Year");
+                }
+            }
+        });
+    }
+
+    private boolean isLeap(int year){
+        if( (year %4==0) && (year % 100 != 0) || (year%400==0)){
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
 
     }
