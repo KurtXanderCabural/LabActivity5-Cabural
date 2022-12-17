6bd41e12e010ff72a01fad4c6273b6729d1a1c19
KurtXanderCabural <Kurtxander.cabural@cit.edu>
2022-12-17 12:18:58 +0800

    Rename SimpleCalc Components

---
+++ src/SimpleCalc/SimpleCalc.java
@@ -0,0 +1,12 @@
+package SimpleCalc;
+
+import javax.swing.*;
+
+public class SimpleCalc {
+    private JPanel panel1;
+    private JTextField tfNumber1;
+    private JComboBox comboBox1;
+    private JButton btnCompute;
+    private JTextField tfNumber2;
+    private JTextField lblResult;
+}
